12039a9e40079d595819a482fc17564a50809d02
takumi <65800363+hatakumi323@users.noreply.github.com>
2021-02-28 23:44:17 +0900

    swiper slider

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+    　loop: true,
+      direction:'vertical',
+      navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+      }
+    });
+    
