ac16ebffc8f6facbab953058ccc5fd7d51bac725
takumi <65800363+hatakumi323@users.noreply.github.com>
2021-03-26 19:21:37 +0900

    sp_slider

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-var swiper = new Swiper('.swiper-container', {
-    loop: true,
-      direction:'vertical',
-      navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-      }
-    });
-    
+let mySwiper = new Swiper('.swiper-container', {
+  navigation: {  //ナビゲーションのオプション（矢印ボタンの要素を指定）
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
