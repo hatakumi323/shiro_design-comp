a243ffd978341d063e3de3f0d706355639f1b501
takumi <65800363+hatakumi323@users.noreply.github.com>
2021-03-26 21:57:17 +0900

    sp_contact

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 let mySwiper = new Swiper('.swiper-container', {
+  
+  autoplay: {
+    delay: 6000,
+  },
+  
+  loop: true,
+  
+  
   navigation: {  //ナビゲーションのオプション（矢印ボタンの要素を指定）
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
