bea87e30fe23b53a768a6ecadb12dfc455722230
takumi <65800363+hatakumi323@users.noreply.github.com>
2021-03-27 18:00:53 +0900

    scroll header

--- js/script.js
+++ js/script.js
@@ -12,3 +12,28 @@ let mySwiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   }
 });
+
+// ヘッダーを取得
+const header = document.getElementById("header");
+
+// ヘッダーの高さを取得
+const hH = header.clientHeight;
+
+// 現在地を示す変数を定義
+let pos = 0;
+
+const onScroll = () => {
+  console.log('scroll');
+  // スクロール位置がヘッダーの高さ分より大きい場合にclass名を追加し、そうでない場合にclass名を削除
+  if(pos > hH) {
+    header.classList.add('header--unpinned');
+  } else {
+    header.classList.remove('header--unpinned');
+  }
+};
+
+window.addEventListener("scroll", () => {
+  // スクロールするごとにpos（現在地）の値を更新
+  pos = window.scrollY;
+  onScroll();
+});
